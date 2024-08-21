ea7ceeaa86e43c74f4c0e0d84b5500059ad97717
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-08-21 15:30:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+		let fn = document.getElementsByName("fname")[0].value;
+		let ln = document.getElementsByName("lname")[0].value;
 
+		alert(fn + " " + ln);
 }
